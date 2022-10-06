c7eb4206153a24200c95be51662bfeb35b0883a5
Alex J <alexandergrahambell707@gmail.com>
2022-10-06 13:05:23 -0500

    App update

---
+++ Hangman/extend.py
@@ -0,0 +1,10 @@
+def separate_chars(string):
+    list = [i for i in string]
+    return list
+
+def list2str(list):
+    string = " ".join(list)
+    return string
+
+def diffconverter(difficulty):
+    return -.5*difficulty + 2.5
